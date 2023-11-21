5c72a180947b0f66ebd4593ff52ba988f16b431c
devesh kumar meena <129925418+deevsh9672@users.noreply.github.com>
2023-11-21 18:08:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const calculate=()=>{
+/*const calculate=()=>{
   const originalPrice = number(document.getElementById("originalPrice").value);
   const discountedPrice= number(document.getElementById("discountedPrice").value);
 
   const discountPrecentage = (((originalPrice-discountedPrice)/originalPrice*100).toFixed(2));
   document.getElementById("result").innerText=discountPrecentage;
 
+};*/
+const calculate = () => {
+  const originalPrice = Number(document.getElementById("originalPrice").value);
+  const discountedPrice = Number(
+    document.getElementById("discountedPrice").value
+  );
+  const discountPercentage = (((originalPrice - discountedPrice) / originalPrice) *100).toFixed(2);
+
+  document.getElementById("result").innerText = discountPercentage;
 };
